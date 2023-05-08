dcbf28e27a92ea344d83f087358209cb55210c1a
agorbacheva-y <agorbacheva.y@gmail.com>
2023-05-08 15:18:52 +0200

    add body parts to image map area

--- src/components/BoyOne.js
+++ src/components/BoyOne.js
@@ -28,12 +28,77 @@ const BoyOne = () => {
         />
         
         <area 
-          shape="poly"
+          shape="circle"
           alt="left-hand" 
-          coords="75,292,50,286,50,300,32,299,45,305,39,314,58,313,57,328,70,319,78,322,80,307"
+          coords="54,310,29"
           onClick={() => alert("left hand")}
           style={{ cursor:"pointer"}}
         />
+
+        <area 
+          shape="circle"
+          alt="right-hand" 
+          coords="343,300,28" 
+          onClick={() => alert("right hand")}
+          style={{ cursor:"pointer"}}
+        />
+
+        <area 
+          shape="poly"
+          alt="left-arm" 
+          coords="71,289,86,309,147,295,139,264"
+          onClick={() => alert("left arm")}
+          style={{ cursor:"pointer"}}
+        />
+
+        <area 
+          shape="poly"
+          alt="right-arm" 
+          coords="259,293,268,264,327,282,320,302"
+          onClick={() => alert("right arm")}
+          style={{ cursor:"pointer"}}
+        />
+        
+        <area 
+          shape="poly"
+          alt="body"
+          coords="146,263,259,263,263,398,144,400"
+          onClick={() => alert("body")}
+          style={{ cursor:"pointer"}}
+        />
+
+        <area 
+          shape="poly"
+          alt="left-leg"
+          coords="144,399,204,404,194,465,140,460"
+          onClick={() => alert("left leg")}
+          style={{ cursor:"pointer"}}
+        />
+
+        <area 
+          shape="poly"
+          alt="right-leg"
+          coords="208,407,264,400,270,457,219,467"
+          onClick={() => alert("right leg")}
+          style={{ cursor:"pointer"}}
+        />
+
+        <area 
+          shape="poly"
+          alt="left-foot"
+          coords="137,476,178,482,182,465,141,461"
+          onClick={() => alert("left foot")}
+          style={{ cursor:"pointer"}}
+        />
+
+        <area 
+          shape="poly"
+          alt="right-foot"
+          coords="221,467,222,478,274,473,272,457"
+          onClick={() => alert("right foot")}
+          style={{ cursor:"pointer"}}
+        />
+
       </map>
     </div>
   );
