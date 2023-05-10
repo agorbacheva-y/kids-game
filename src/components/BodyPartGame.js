06efded4ae14ce46a70c826fa4ee128f08abd6ba
agorbacheva-y <agorbacheva.y@gmail.com>
2023-05-10 21:48:41 +0200

    svg set to fixed width/height

--- src/components/BodyPartGame.js
+++ src/components/BodyPartGame.js
@@ -1,11 +1,9 @@
-import BoyOne from "./BoyOne";
 import BoySvg from "./BoySvg";
 
 const BodyPartGame = ({ onClick }) => {
   return (
     <div className="container">
       <h1>body part game</h1>
-      <BoyOne onClick={onClick} />
       <BoySvg />
     </div>
   );
--- src/components/BoySvg.js
+++ src/components/BoySvg.js
@@ -18,40 +18,40 @@ const BoySvg = () => {
         >
           <a className="svg-click">
             <rect 
-              x="275" 
-              y="350" 
-              width="425" 
-              height="250" 
+              x="210" 
+              y="300" 
+              width="550" 
+              height="300" 
               onClick={() => alert("face")}
             />
           </a> 
 
           <a className="svg-click">
             <rect 
-              x="370" 
+              x="360" 
               y="600" 
-              width="260" 
-              height="335" 
+              width="295" 
+              height="420" 
               onClick={() => alert("body")}
             />
           </a>    
 
           <a className="svg-click">
             <rect 
-              x="160" 
-              y="650" 
-              width="110" 
-              height="110" 
+              x="80" 
+              y="665" 
+              width="140" 
+              height="140" 
               onClick={() => alert("left hand")}
             />
           </a> 
 
           <a className="svg-click">
             <rect 
-              x="725" 
-              y="630" 
-              width="110" 
-              height="110" 
+              x="790" 
+              y="645" 
+              width="140" 
+              height="140" 
               onClick={() => alert("right hand")}
             />
           </a> 
