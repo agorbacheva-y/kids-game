d2257842be93756a8b5531b20c0fa23781f4739a
agorbacheva-y <agorbacheva.y@gmail.com>
2023-05-10 17:05:09 +0200

    add body parts to svg

--- src/components/BoySvg.js
+++ src/components/BoySvg.js
@@ -1,6 +1,11 @@
 import Boy from "../images/Boy.svg";
 
 const BoySvg = () => {
+  const onClick = (e) => {
+    e.preventDefault();
+    onClick();
+  }
+
   return (
       <div className="container">
         <img src={Boy} className="svg-boy"/>
@@ -11,18 +16,46 @@ const BoySvg = () => {
           x="0" 
           y="0"
         >
-          <a>
+          <a className="svg-click">
             <rect 
               x="275" 
               y="350" 
-              fill="green" 
-              opacity="0.5" 
               width="425" 
               height="250" 
               onClick={() => alert("face")}
-              style={{ cursor:"pointer"}}
             />
-          </a>      
+          </a> 
+
+          <a className="svg-click">
+            <rect 
+              x="370" 
+              y="600" 
+              width="260" 
+              height="335" 
+              onClick={() => alert("body")}
+            />
+          </a>    
+
+          <a className="svg-click">
+            <rect 
+              x="160" 
+              y="650" 
+              width="110" 
+              height="110" 
+              onClick={() => alert("left hand")}
+            />
+          </a> 
+
+          <a className="svg-click">
+            <rect 
+              x="725" 
+              y="630" 
+              width="110" 
+              height="110" 
+              onClick={() => alert("right hand")}
+            />
+          </a> 
+
         </svg>
       </div>
   );
