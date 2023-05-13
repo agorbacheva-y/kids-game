eddf99777dba52a58896829431b44710b0bea623
ChaliMsani <chalisani@hotmail.com>
2023-05-13 19:32:19 +0200

    the timer is displayed with a button

--- src/components/ShapeGame.js
+++ src/components/ShapeGame.js
@@ -1,8 +1,44 @@
-import { useState, useEffect } from "react";
+import { useState, useRef, useEffect } from "react";
 import Button from "react-bootstrap/Button";
-import { Link } from "react-router-dom";
 
 const ShapeGame = () => {
+    // this is the timer function
+   const [timer, setTimer] =  useState ("00:00:00")
+   const Ref = useRef()
+
+   function getTimerRemaining(e) {
+    const total = Date.parse(e) - Date.parse(new Date())
+    const seconds = Math.floor((total / 1000) % 60);
+
+    return {total, seconds}
+   }
+   function startTimer(e) {
+    let {total, seconds} = getTimerRemaining(e);
+    if(total >= 0) {
+        setTimer((seconds > 10 ? seconds : '0' + seconds)
+        )
+    }
+   }
+   function clearTimer (e) {
+    setTimer("00:00:10")
+    if(Ref.current) clearInterval(Ref.current)
+    const id = setInterval(()=> {
+        startTimer(e)
+    }, 100)
+    Ref.current = id;
+   }
+   function getDeadTime() {
+     let deadline = new Date(); 
+     deadline.setSeconds(deadline.getSeconds() + 10)
+     return deadline;
+   }
+
+   function Reset() {
+     clearTimer(getDeadTime())
+   }
+   useEffect(()=> {
+    clearTimer(getDeadTime())
+   }, []);
 
     return (
       <div className="container"> 
@@ -11,7 +47,8 @@ const ShapeGame = () => {
             is similar to the object that is displayed on the screen in your home.
         </p>
         <p>Start the timer when you are ready!</p>
-        <Button size="lg">
+        <h2>{timer}</h2>
+        <Button size="lg" onClick={Reset}>
             Start Timer
          </Button>
       </div>
