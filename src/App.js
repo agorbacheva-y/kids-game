9d6d1450eecd0d13c4083537e1013409a3663e49
ChaliMsani <chalisani@hotmail.com>
2023-05-13 15:43:26 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Header from "./components/Header";
 import Register from "./components/Register";
+import ShapeGame from "./components/ShapeGame";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/register" exact element={<Register />} />
       </Routes>
+      <ShapeGame></ShapeGame>
     </BrowserRouter>
   );
 };
---
+++ src/components/ShapeGame.js
@@ -0,0 +1,9 @@
+const ShapeGame = () => {
+    return (
+      <div> 
+        <h1>Shape Game</h1>
+      </div>
+    );
+};
+
+export default ShapeGame;
