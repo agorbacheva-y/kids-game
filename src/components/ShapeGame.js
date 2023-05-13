6a9d60842474347c0e9660bdfe51584af1936ae3
ChaliMsani <chalisani@hotmail.com>
2023-05-13 20:18:47 +0200

    added the images

--- src/components/ShapeGame.js
+++ src/components/ShapeGame.js
@@ -3,7 +3,7 @@ import Button from "react-bootstrap/Button";
 
 const ShapeGame = () => {
     // this is the timer function
-   const [timer, setTimer] =  useState ("00:00:00")
+   const [timer, setTimer] =  useState ("0")
    const Ref = useRef()
 
    function getTimerRemaining(e) {
@@ -15,21 +15,21 @@ const ShapeGame = () => {
    function startTimer(e) {
     let {total, seconds} = getTimerRemaining(e);
     if(total >= 0) {
-        setTimer((seconds > 10 ? seconds : '0' + seconds)
+        setTimer((seconds > 1 ? seconds : '0' + seconds)
         )
     }
    }
    function clearTimer (e) {
-    setTimer("00:00:10")
+    setTimer("0")
     if(Ref.current) clearInterval(Ref.current)
     const id = setInterval(()=> {
         startTimer(e)
-    }, 100)
+    }, 10)
     Ref.current = id;
    }
    function getDeadTime() {
      let deadline = new Date(); 
-     deadline.setSeconds(deadline.getSeconds() + 10)
+     deadline.setSeconds(deadline.getSeconds() + 15)
      return deadline;
    }
 
@@ -43,10 +43,11 @@ const ShapeGame = () => {
     return (
       <div className="container"> 
         <h1>Shape Game</h1>
-        <p>You have 10 seconds to find an object that 
-            is similar to the object that is displayed on the screen in your home.
+        <p>You have 15 seconds to find an object that 
+            is similar <br/> to the object that is displayed on the screen in your home.
         </p>
-        <p>Start the timer when you are ready!</p>
+        
+        <h5>Start the timer when you are ready!</h5>
         <h2>{timer}</h2>
         <Button size="lg" onClick={Reset}>
             Start Timer
@@ -58,4 +59,3 @@ const ShapeGame = () => {
 
 export default ShapeGame;
 
-// link to start the timer
