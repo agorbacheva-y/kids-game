fc9ab05e2ff74ae7770a9cd5656f7c1c8c9cece5
agorbacheva-y <agorbacheva.y@gmail.com>
2023-05-14 15:18:31 +0200

    remove confetti test

--- src/components/BodyPartGame.js
+++ src/components/BodyPartGame.js
@@ -1,16 +1,12 @@
 import BoySvg from "./BoySvg";
-import Confetti from "react-confetti";
 
 const BodyPartGame = () => {
   return (
     <div className="container">
       <h1>body part game</h1>
       <BoySvg />
-      <Confetti />;
     </div>
   );
 };
 
 export default BodyPartGame;
-
-//Confetti placed here to test - it works!
