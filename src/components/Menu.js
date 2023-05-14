6f58b59c3c568e88caf2b975542b8bba2013759b
ChaliMsani <chalisani@hotmail.com>
2023-05-14 21:57:37 +0200

    added a react-routerdom link with the closebtn from bootrsrap

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,14 +1,14 @@
+import React from "react";
 import ReusableButton from "./ReusableButton";
 import CloseButton from 'react-bootstrap/CloseButton';
-import Home from "./Home";
-
-
+import { Link } from "react-router-dom";
 
 const Menu = () => {
     return (
      <div className="container"> 
-      <CloseButton className="closebtn"></CloseButton>
-
+    
+         <Link to="/"> <CloseButton className="closebtn"></CloseButton> </Link>
+    
         <h1>Choose a Game</h1>
         <ReusableButton>Body Parts</ReusableButton>
       <ReusableButton>Find objects</ReusableButton>
