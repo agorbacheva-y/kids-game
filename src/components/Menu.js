125a07939534cd9755a75de4809685f4f110c046
ChaliMsani <chalisani@hotmail.com>
2023-05-14 22:04:46 +0200

    added links to the game buttons

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -7,14 +7,24 @@ const Menu = () => {
     return (
      <div className="container"> 
     
-         <Link to="/"> <CloseButton className="closebtn"></CloseButton> </Link>
-    
+         <Link to="/"> 
+           <CloseButton className="closebtn"></CloseButton> 
+         </Link>
+         
         <h1>Choose a Game</h1>
-        <ReusableButton>Body Parts</ReusableButton>
-      <ReusableButton>Find objects</ReusableButton>
-      <ReusableButton>Drag & drop</ReusableButton>
+        <ReusableButton>
+            Body Parts <Link to="bodypartsgame"/>
+        </ReusableButton>
+        <ReusableButton>
+        Find objects <Link to="findobjectgame"/>
+        </ReusableButton>
+      <ReusableButton>
+        Drag & drop <Link to="dragndropgame"/>
+        </ReusableButton>
      </div>
     );
 };
 
 export default Menu;
+
+// When the games are done link to it in each button
