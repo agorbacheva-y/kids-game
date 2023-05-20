ec9c9af9ead18d6e564e7ee7d923031e3fdbb9ca
ChaliMsani <chalisani@hotmail.com>
2023-05-21 01:45:00 +0200

    all the functions and states set

--- src/components/InfoForParents.js
+++ src/components/InfoForParents.js
@@ -5,8 +5,23 @@ import { Link } from "react-router-dom";
 
 
 const InfoForParents = () => {
+    const [modalOpen, setModalOpen] = useState(false);
+
+    const handleOpenModal = () => {
+        setModalOpen(true);
+    };
+
+    const handleCloseModal = () => {
+        setModalOpen(false);
+    }
     return (
-       <Modal />
+        <>
+        <ReusableButton onClick={handleOpenModal}>
+            {/* This is the onclick that opens the modal */}
+
+        </ReusableButton>
+        <Modal />
+        </>
     );
 };
 
