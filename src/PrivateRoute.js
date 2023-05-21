fb875dbb16d171d96ad0a60f9ba91f4ace82a6eb
agorbacheva-y <48282136+agorbacheva-y@users.noreply.github.com>
2023-05-21 21:45:09 +0200

    Comment PrivateRoute.js

--- src/PrivateRoute.js
+++ src/PrivateRoute.js
@@ -7,3 +7,7 @@ const PrivateRoute = ({ children }) => {
 };
 
 export default PrivateRoute;
+
+// when importing {authenticate} at the top, we do not have a data folder so please update the name to where it is imported from
+
+// your login component is called LoginFeature.js so pls change the link in <Navigate> to "to="/loginfeature"
