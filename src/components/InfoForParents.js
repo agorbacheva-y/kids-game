f13e5110762e3609105a980293b3999075118672
ChaliMsani <chalisani@hotmail.com>
2023-05-21 02:30:06 +0200

    deleted changes

--- src/components/InfoForParents.js
+++ src/components/InfoForParents.js
@@ -1,27 +1,8 @@
 import Modal from "./Modal";
-import { useState } from "react";
-import ReusableButton from "./ReusableButton";
-import { Link } from "react-router-dom";
-
 
 const InfoForParents = () => {
-    const [modalOpen, setModalOpen] = useState(false);
-
-    const handleOpenModal = () => {
-        setModalOpen(true);
-    };
-
-    const handleCloseModal = () => {
-        setModalOpen(false);
-    }
     return (
-        <>
-        <ReusableButton onClick={handleOpenModal}>
-            {/* This is the onclick that opens the modal */}
-
-        </ReusableButton>
-        <Modal />
-        </>
+       <Modal />
     );
 };
 
