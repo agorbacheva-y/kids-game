d746d5d9855e0ae67d925b1332da6136fb68bdef
ChaliMsani <chalisani@hotmail.com>
2023-05-22 21:36:52 +0200

    using the button instead of reusablebutton

--- src/components/InfoForParents.js
+++ src/components/InfoForParents.js
@@ -1,19 +1,39 @@
 import Modal from "./Modal";
-import ReusableButton from "./ReusableButton";
 import CloseButton from 'react-bootstrap/CloseButton';
+import { useState } from "react";
 
-const InfoForParents = () => {
+const InfoForParents = ({close}) => {
+  const [status, setStatus] = useState(false);
+  
+
     return (
        <div className="container">
-        <Modal> 
-            <CloseButton className="close" onClick={close}/>
-            <h2>For parents!</h2>
-            <p>
-            Welcome, parents! SmartKids is a fun and educational app designed to stimulate your child's learning and development. With engaging games focused on cognitive skills, shape recognition, body awareness, and furniture arrangement, our app offers a delightful way for your child to learn while having fun. Each game is thoughtfully crafted to promote problem-solving, creativity, and critical thinking. Through interactive gameplay and gentle guidance, your child can enhance their cognitive abilities, spatial awareness, and attention to detail. Join us in empowering your child's growth and watch them thrive in a safe and engaging digital environment. Start their learning adventure with SmartKids today!
-            </p>
-            
-        </Modal>
-      <ReusableButton className="reuse-btn">For parents</ReusableButton>
+       
+            {status && (
+            <Modal>
+              <div className="container">
+                <CloseButton className="close" onClick={close}/>
+                   <h2>For parents!</h2>
+                    <p>
+                     Welcome, parents! SmartKids is a fun and educational 
+                     app designed to stimulate your child's learning and 
+                     development. With engaging games focused on cognitive
+                     skills, shape recognition and body awareness our app
+                     offers a delightful way for your child to learn while 
+                     having fun. Each game is thoughtfully crafted to promote 
+                     problem-solving, creativity, and critical thinking. 
+                     Through interactive gameplay and gentle guidance, your 
+                     child can enhance their cognitive abilities, spatial 
+                     awareness, and attention to detail. Join us in empowering 
+                     your child's growth and watch them thrive in a safe and 
+                     engaging digital environment. Start their learning adventure 
+                     with SmartKids today!
+                   </p> 
+               </div> 
+            </Modal>
+            )}
+     
+        <button onClick={() => setStatus(true)} className="reuse-btn">For parents</button>
 
        </div>
     );
