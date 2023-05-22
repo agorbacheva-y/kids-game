ee20ae5cd8ff538a9c4eecd763bdae92688da41d
ChaliMsani <chalisani@hotmail.com>
2023-05-22 21:11:49 +0200

    added title and text to modal

--- src/components/InfoForParents.js
+++ src/components/InfoForParents.js
@@ -6,7 +6,12 @@ const InfoForParents = () => {
     return (
        <div className="container">
         <Modal> 
-            <CloseButton />
+            <CloseButton className="close" onClick={close}/>
+            <h2>For parents!</h2>
+            <p>
+            Welcome, parents! SmartKids is a fun and educational app designed to stimulate your child's learning and development. With engaging games focused on cognitive skills, shape recognition, body awareness, and furniture arrangement, our app offers a delightful way for your child to learn while having fun. Each game is thoughtfully crafted to promote problem-solving, creativity, and critical thinking. Through interactive gameplay and gentle guidance, your child can enhance their cognitive abilities, spatial awareness, and attention to detail. Join us in empowering your child's growth and watch them thrive in a safe and engaging digital environment. Start their learning adventure with SmartKids today!
+            </p>
+            
         </Modal>
       <ReusableButton className="reuse-btn">For parents</ReusableButton>
 
