c0f9ab3640fd4a9c59e5822d9b7d6ea6922535e3
agorbacheva-y <agorbacheva.y@gmail.com>
2023-05-23 09:33:19 +0200

    remove voice change function

--- src/components/BodyPartGame/TextToSpeech.js
+++ src/components/BodyPartGame/TextToSpeech.js
@@ -4,21 +4,15 @@ import { BsFillPlayCircleFill, BsFillStopCircleFill } from "react-icons/bs";
 const TextToSpeech = ({ text }) => {
   // state to store text, voice, speed for speech synthesis
   const [ utterance, setUtterance ] = useState("");
-  const [ voice, setVoice ] = useState("");
   const [ rate, setRate ] = useState(1);
 
   useEffect(() => {
     const synth = window.speechSynthesis;
     // create SpeechSynthesisUtterance object (text = what will be read)
     const u = new SpeechSynthesisUtterance(text);
-
-    // get voices array
-    const voices = synth.getVoices();
     
     // initialize utterance and voice state
     setUtterance(u);
-    setVoice(voices[0]);
-    // set female voice
     setRate(0.7); // slower speed for targeted audience
 
     return () => {
@@ -31,7 +25,6 @@ const TextToSpeech = ({ text }) => {
   const handlePlay = () => {
     const synth = window.speechSynthesis;
 
-    utterance.voice = voice;
     utterance.rate = rate;
     synth.speak(utterance);
 
@@ -39,21 +32,16 @@ const TextToSpeech = ({ text }) => {
     console.log(utterance);
   };
 
-  // functionto stop speaking
+  // function to stop speaking
   const handleStop = () => {
     const synth = window.speechSynthesis;
 
     synth.cancel();
   };
 
-  const handleVoiceChange = () => {
-    const voices = window.speechSynthesis.getVoices();
-    setVoice(voices.filter(function(voice) { return voice.name === 'Google UK English Female'; })[0] );
-  };
-
   return (
     <div> 
-      <button onClick={() => {handlePlay(); handleVoiceChange() }} className="speech-btn"><BsFillPlayCircleFill className="speech-btn-icon"/></button>
+      <button onClick={handlePlay} className="speech-btn"><BsFillPlayCircleFill className="speech-btn-icon"/></button>
       <button onClick={handleStop} className="speech-btn"><BsFillStopCircleFill className="speech-btn-icon"/></button>
     </div>
   );
@@ -67,4 +55,5 @@ export default TextToSpeech;
 // could not use time interval or event listener or pg reload to populate
 // i assume because play button is in modal. 
 
-//setVoice(voices.filter(function(voice) { return voice.name === 'Google UK English Female'; })[0] );
+// for female voice:
+// setVoice(voices.filter(function(voice) { return voice.name === 'Google UK English Female'; })[0] );
